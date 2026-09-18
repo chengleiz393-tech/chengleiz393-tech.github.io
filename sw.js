4f35e851f6006705983272e7a0b73c9c482367bc
chengleiz393-tech <chengleiz393@gmail.com>
2026-09-18 22:32:11 +0800

    Site updated: 2026-09-18 22:32:11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1789741883844";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1789741927802";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
